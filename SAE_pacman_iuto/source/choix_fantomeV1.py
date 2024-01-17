c810b9db8b972738d3bb7ee47cdce2e7ab8fc097
farid-cheddad <143810134+farid-cheddad@users.noreply.github.com>
2024-01-17 12:07:15 +0100

    Update choix_fantomeV1.py

--- SAE_pacman_iuto/source/choix_fantomeV1.py
+++ SAE_pacman_iuto/source/choix_fantomeV1.py
@@ -79,4 +79,53 @@ def test_direction_j_pacman_fantome():
     choix = max((E,O),key=lambda dir: dir["fantomes"])
     assert(choix == E)
     print(choix)
+
+
+def direction_j_pacman_fantome(le_plateau, le_joueur):
+    """Définit en fonction de la position de mon pacman sur le plateau, quelle direction
+    prendre pour s’éloigner du fantôme le plus proche 
+
+    Args:
+        le_plateau (dict): Le plateau considéré
+        Joueur (dict): Un dictionnaire représentant le joueur
+
+    Returns:
+        (str): une chaine de caractères indiquant les directions possible 
+    """
+    dir_pos = plateau.directions_possibles(le_plateau, joueur.get_pos_pacman(le_joueur))
+    dico_analyse = dict()
+    for direction in dir_pos:
+        dico_analyse[direction] = plateau.analyse_plateau(le_plateau, joueur.get_pos_pacman(le_joueur), direction, plateau.get_nb_lignes(le_plateau))
+        
+    choix = min(dico_analyse, key = lambda direct : dico_analyse[direct]["fantomes"])
+
+    if choix is None:
+         return random.choice(dir_pos)
+    else:
+        if choix == "N" and "S" in dir_pos:
+            direction = "S"
+        elif choix == "S" and "N" in dir_pos:
+            direction = "N"
+        elif choix == "E" and "O" in dir_pos:
+            direction = "O"
+        elif choix == "O" and "E" in dir_pos:
+            direction = "E"
+        else:
+            direction = random.choice(dir_pos)
+             
+    return direction 
+              
+
+def test_direction_j_pacman_fantome():
+    with open("cartes/test1.txt") as fic:
+            plateau1=fic.read()
+    plateau1 = plateau.Plateau(plateau1)
+    E = plateau.analyse_plateau(plateau1,(7,5),"E",plateau.get_nb_lignes(plateau1))
+    print(plateau1)
+    print('ici',E)
+    O = plateau.analyse_plateau(plateau1,(7,5),"O",plateau.get_nb_lignes(plateau1))
+    print(O)
+    choix = max((E,O),key=lambda dir: dir["fantomes"])
+    assert(choix == E)
+    print(choix)
     
